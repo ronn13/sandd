15cf9129b6c4dba31bf87fe958497bb1798915a0
ronn13 <delyon.ronnie@gmail.com>
2016-03-22 07:41:46 +0300

    model changes

--- adm/forms.py
+++ adm/forms.py
@@ -1,18 +1,3 @@
 from django import forms
 from models import *
 
-class ProductsForm(forms.ModelForm):
-    STOCK_TYPES = (
-        ('opening', 'Opening Stock'),
-        ('closing', 'Closing Stock'),
-        ('new', 'New Stock')
-    )
-    
-    shampoo = forms.IntegerField(initial=0)
-    hairgel = forms.IntegerField(initial=0)
-    relaxer = forms.IntegerField(initial=0)
-    stock_type = forms.ChoiceField(choices=STOCK_TYPES, widget=forms.RadioSelect())
-    
-    class Meta:
-        model = Products 
-        fields = ('store','shampoo','hairgel','relaxer','stock_type')
--- adm/models.py
+++ adm/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Store(models.Model):
     store_name = models.CharField(max_length=250)
     agent = models.ForeignKey('Agent')
-    
+       
     def __str__(self):
 		return self.store_name
 
@@ -14,9 +14,20 @@ class Agent(models.Model):
 	def __str__(self):
 		return self.agent_names
 
-class Products(models.Model):
-    name = models.CharField(max_length=20)
+class Product(models.Model):
+    name = models.CharField(max_length=20, default="product name")
     unit_price = models.IntegerField()
     
     def __str__(self):
         return self.name
+
+class Stock(models.Model):
+    CHOICES = ((1, 'Opening Stock'),
+        (2, 'Closing stock'),
+        (3, 'New stock'),)
+    product = models.ForeignKey('Product')
+    count = models.IntegerField()
+    store_name = models.ForeignKey('Store')
+    stock_type = models.CharField(max_length=20, choices=CHOICES)
+    stock_time = models.DateTimeField(auto_now_add=True)
+    
