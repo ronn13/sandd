a440509884158e5f3e6a021fcd6b8e7e3cf5edbb
ronn13 <delyon.ronnie@gmail.com>
2016-04-15 02:12:20 +0300

    added model methods to count daily, monthly and yearly totals. not yet changed view and template to view these changes but should be an easy task

--- adm/models.py
+++ adm/models.py
@@ -1,32 +1,40 @@
 import datetime
 from django.db import models
+from django.db.models import F, Sum
 
 class Region(models.Model):
     name = models.CharField(max_length=50)
     
     def __str__(self):
         return self.name
+        
+    def __unicode__(self):
+        return '%s' % (self.name)
 
 class Location(models.Model):
     name = models.CharField(max_length=50)
     region = models.ForeignKey('Region')
     
-    def __str__(self):
-        return self.name
+    # def __str__(self):
+    #     return self.name
+        
+    def __unicode__(self):
+        return unicode(self.name)
 
 class Agent(models.Model):
 	agent_names = models.CharField(max_length=50)
 	
 	def __str__(self):
 		return self.agent_names
+    
 
 class Store(models.Model):
     store_name = models.CharField(max_length=250)
-    location = models.ForeignKey('Location')
+    store_location = models.ForeignKey('Location')
     agent = models.ForeignKey('Agent')
        
-    def __str__(self):
-		return self.store_name
+    def __unicode__(self):
+        return unicode(self.name)
 
 class Product(models.Model):
     name = models.CharField(max_length=20, default="product name")
@@ -34,17 +42,19 @@ class Product(models.Model):
     
     def __str__(self):
         return self.name
+        
+    def __unicode__(self):
+        return '%s' % (self.name)
 
 class StockManager(models.Manager):
-    def day_stock(self, keyword):
-        q1 = self.filter(store__name=keyword)
-        #q2 = q1.filter(self.stock_time.date() = datetime.datetime.now().date()).aggregate(Sum(count()))
+    def year_count(self, keyword):
+        return Stock.objects.filter(store__store_name=keyword).filter(stock_time__year=datetime.datetime.today().date().year).aggregate(Sum('stock_count'))
+    #for month and day, datetime returns single digits without the leading zero. add if to add it if count is less than 2
+    def month_count(self, keyword):
+        return Stock.objects.filter(store__store_name=keyword).filter(stock_time__month=datetime.datetime.today().date().month).aggregate(Sum('stock_count'))
         
-    def week_stock(self, keyword):
-        return self.filter(store_name=keyword).aggregate(Sum(count()))
-        
-    def year_stock(self, keyword):
-        return self.filter(store_name=keyword).aggregate(Sum(count()))
+    def day_count(self, keyword):
+        return Stock.objects.filter(store__store_name=keyword).filter(stock_time__day=datetime.datetime.today().date().day).aggregate(Sum('stock_count'))
 
 class Stock(models.Model):
     CHOICES = (('1', 'Opening Stock'),
@@ -55,7 +65,6 @@ class Stock(models.Model):
     store = models.ForeignKey('Store')
     stock_type = models.CharField(max_length=20, choices=CHOICES)
     stock_time = models.DateTimeField(auto_now_add=True)
-    objects = StockManager()   
-        
-    # def __str__(self):
-    #     return self.prodct
+    objects = StockManager()
+    
+    
--- adm/views.py
+++ adm/views.py
@@ -1,4 +1,5 @@
 #for deployment to openshift
+import datetime
 from django.shortcuts import render
 from django.contrib import messages
 from forms import *
@@ -6,16 +7,33 @@ from models import *
 
 def store_totals(request, region=None):
     stores = []
+    day_stock = {}
+    month_stock = {}
+    year_stock = {}
     if region is None:
         region='Central'    
     locations = Location.objects.filter(region__name=region)
-    for location in locations:
-        for shops in Store.objects.filter(location__name=location):
-            stores.append(shops)
-    this_
+    for ltn in locations:
+        for shop in Store.objects.filter(store_location__name=ltn):
+            stores.append(shop)
+            index = shop.store_name
+            day_stock[index] = 0
+            month_stock[index] = 0
+            year_stock[index] = 0
+            for stock in Stock.objects.filter(store__store_name=shop.store_name):
+                if stock.stock_time.date().year == datetime.datetime.now().date().year: 
+                    year_stock[index] = year_stock[index] + stock.stock_count                    
+                if stock.stock_time.date().month == datetime.datetime.now().date().month: 
+                    month_stock[index] = month_stock[index] + stock.stock_count                    
+                if stock.stock_time.date().day == datetime.datetime.now().date().day: 
+                    day_stock[index] = day_stock[index] + stock.stock_count
+                    
     context = {
         'stores':stores,
-        'region':region
+        'region':region,
+        'day_stock':day_stock,
+        'month_stock':month_stock,
+        'year_stock':year_stock 
     }
     return render(request, 'store_totals.html', context)        
 
